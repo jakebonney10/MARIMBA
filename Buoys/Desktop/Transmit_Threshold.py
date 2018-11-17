b317b69432308818caeb588d716ec656958716af
jakebonney10 <43123441+jakebonney10@users.noreply.github.com>
2018-11-16 20:54:11 -0500

    Update Transmit_Threshold.py

--- Buoys/Desktop/Transmit_Threshold.py
+++ Buoys/Desktop/Transmit_Threshold.py
@@ -15,29 +15,27 @@ source = "/home/pi/Documents/Data/"
 drive = "/media/pi/usb/1/"
 d_time = 0
 
-#def butter_bandpass(lowcut, highcut, fs, order=5):
-#    nyq = 0.5 * fs
-#    low = lowcut / nyq
-#    high = highcut / nyq
-#    b, a = butter(order, [low, high], btype='band')
-#   return b, a
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+   return b, a
 
-#def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-#    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-#    y = lfilter(b, a, data)
-#    return y	
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y	
 
 def threshold(signal,fs,lowcut,highcut,threshold):
 	v = np.genfromtxt(signal, delimiter=",",skip_header=5, usecols=(0), unpack=True)
-	#v_filtered = butter_bandpass_filter(v, lowcut, highcut, fs, order=6)
-	v_fft= np.abs(fft(v))
+	v_filtered = butter_bandpass_filter(v, lowcut, highcut, fs, order=6)
+	v_fft= np.abs(fft(v_filtered))
 	if (v_fft >= threshold).any() == True:
-		return "Possible Whale Detected: Send File"
 		scp = "sudo sshpass -p '******' scp /home/pi/Documents/Data/%s.txt.bz2 whale-srv@******:~/Whale_Srv/Incoming/1/" % d_time
 		os.system(scp)
 		print("FILE SENT!")
 	else:
-		return "No Whale Detected: Do Not Send File"
 		print("FILE NOT SENT!")	
 	
 def transmit():
