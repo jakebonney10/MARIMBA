9b94ff27be1cd571b30cb1bfaf9f24835851345a
jakebonney10 <43123441+jakebonney10@users.noreply.github.com>
2018-11-16 20:05:55 -0500

    Update and rename Transmit_Threshold4.py to Transmit_Threshold.py
    Commented out filtering the signal to test if the threshold works. Renamed variables to more clear names.

--- Buoys/Desktop/Transmit_Threshold.py
+++ Buoys/Desktop/Transmit_Threshold.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 import os
 import numpy as np
-from scipy.signal import freqz, butter, lfilter
+#from scipy.signal import freqz, butter, lfilter
 from scipy.fftpack import fft
 
 GPIO.setwarnings(False)
@@ -15,30 +15,30 @@ source = "/home/pi/Documents/Data/"
 drive = "/media/pi/usb/1/"
 d_time = 0
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
-    return b, a
+#def butter_bandpass(lowcut, highcut, fs, order=5):
+#    nyq = 0.5 * fs
+#    low = lowcut / nyq
+#    high = highcut / nyq
+#    b, a = butter(order, [low, high], btype='band')
+#   return b, a
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y	
+#def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+#    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+#    y = lfilter(b, a, data)
+#    return y	
 
-def threshold(x,fs,lowcut,highcut,threshold):
-	v = np.genfromtxt(x, delimiter=",",skip_header=5, usecols=(0), unpack=True)
-	vf = butter_bandpass_filter(v, lowcut, highcut, fs, order=6)
-	vfft = np.abs(fft(vf))
-	if (vfft >= threshold).any() == True:
-		return 'Possible Whale Detected: Send File'
+def threshold(signal,fs,lowcut,highcut,threshold):
+	v = np.genfromtxt(signal, delimiter=",",skip_header=5, usecols=(0), unpack=True)
+	#v_filtered = butter_bandpass_filter(v, lowcut, highcut, fs, order=6)
+	v_fft= np.abs(fft(v))
+	if (v_fft >= threshold).any() == True:
+		return "Possible Whale Detected: Send File"
 		scp = "sudo sshpass -p '******' scp /home/pi/Documents/Data/%s.txt.bz2 whale-srv@******:~/Whale_Srv/Incoming/1/" % d_time
 		os.system(scp)
-		print('FILE SENT!')
+		print("FILE SENT!")
 	else:
-		return 'No Whale Detected: Do Not Send File'
-		print('FILE NOT SENT!')	
+		return "No Whale Detected: Do Not Send File"
+		print("FILE NOT SENT!")	
 	
 def transmit():
 	d_time = time.time()
